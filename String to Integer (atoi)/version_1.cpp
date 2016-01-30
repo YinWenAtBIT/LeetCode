3dc228143fcac7485b898b2d45e85842c9f74de5
Yin Wen <240083998@qq.com>
2016-01-30 16:42:51 +0800

    发现了一个leetcode未测试到的地方

--- String to Integer (atoi)/version_1.cpp
+++ String to Integer (atoi)/version_1.cpp
@@ -6,7 +6,7 @@ public:
         int max= 0x7fffffff;
         int min= 0x80000000;
         int flagcount =0;
-        int startnum_fiag =1;
+        int startnum_flag =1;
 
         long res =0;
         for(int i=0; i<len; i++)
@@ -14,7 +14,7 @@ public:
             if(str[i]>='0' && str[i]<= '9')
             {
                 res = res*10 + str[i] -'0';
-                startnum_fiag  =0;
+                startnum_flag  =0;
                 
                 if(res > max)
                     if(flag ==1)
@@ -24,21 +24,22 @@ public:
             }
             else
             {
-                if(str[i] == '+' || str[i] == '-')
+                if((str[i] == '+' || str[i] == '-') && startnum_flag ==1)
                 {
                     if(str[i] == '+')
                         flag = 1;
                     else
                         flag = -1; 
                         
-                    startnum_fiag =0;
                     
                     if(flagcount == 0) 
                         flagcount =1;
                     else 
                         return 0;
+                        
+                    startnum_flag =0;
                 }
-                else if(str[i] == ' ' && startnum_fiag)
+                else if(str[i] == ' ' && startnum_flag)
                     continue;
                 else
                     break;
