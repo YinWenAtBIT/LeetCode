48d693dfb1aede6317dae973b00cf4da8f2fcea1
Yin Wen <240083998@qq.com>
2016-02-16 00:02:16 +0800

    在有重复的旋转排序数组中寻找数字

---
+++ Search in Rotated Sorted Array II/version_1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool search(vector<int>& A, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int n = A.size();
+        int left = 0, right = n - 1, mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (A[mid] == target) {
+                return true;
+            }
+            if (A[mid] < A[right]) {
+                if (A[mid] < target && target <= A[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (A[mid] > A[right]) {
+                if (A[left] <= target && target < A[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                right--;
+            }
+        }
+        return false;
+    }
+};
