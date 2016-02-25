49b2ee7add6c72113c5a9000cc788421a8ceb9f4
Yin Wen <240083998@qq.com>
2016-02-25 16:25:57 +0800

    在翻转的排序列表中找最小值

---
+++ Find Minimum in Rotated Sorted Array/README.md
@@ -0,0 +1,6 @@
+#第一百五十三题
+##Find Minimum in Rotated Sorted Array
+在旋转了的已排序数列中寻找最小值
+
+###版本1
+使用二分查找
---
+++ Find Minimum in Rotated Sorted Array/version_1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int len = nums.size();
+        if(!len)
+            return 0;
+        return min_search(nums, 0, len-1);
+    }
+    int min_search(vector<int> &nums, int left, int right)
+    {
+        if(left == right)
+            return nums[left];
+        if(right -left == 1)
+            return min(nums[left], nums[right]);
+        
+        int mid = (left+right)/2;
+        if(nums[mid] > nums[left])
+        {
+            if(nums[mid] >nums[right])
+                return min_search(nums, mid+1, right);
+            else
+                return nums[left];
+        }
+        else
+        {
+            return min_search(nums, left+1, mid);
+        }
+    }
+};
