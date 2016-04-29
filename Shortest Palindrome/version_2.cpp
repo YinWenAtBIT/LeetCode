df6b148f163b9453dee8f5d894ae014c1221bfd1
Yin Wen <240083998@qq.com>
2016-04-29 17:16:42 +0800

    添加最短头部，构成回文串，使用KMP算法

---
+++ Shortest Palindrome/version_2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string r = s;
+        reverse(r.begin(), r.end());
+        string t = s + "#" + r;
+        vector<int> p(t.size(), 0);
+        for (int i = 1; i < t.size(); ++i) {
+            int j = p[i - 1];
+            while (j > 0 && t[i] != t[j]) j = p[j - 1];
+            p[i] = (j += t[i] == t[j]);
+        }
+        return r.substr(0, s.size() - p[t.size() - 1]) + s;
+    }
+};
