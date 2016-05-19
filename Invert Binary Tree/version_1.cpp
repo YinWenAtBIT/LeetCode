641c80f3fcaeef46cb12036baddcd4149045c641
Yin Wen <240083998@qq.com>
2016-05-19 18:12:14 +0800

    翻转二叉树

---
+++ Invert Binary Tree/version_1.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root)
+            return NULL;
+        root ->left = invertTree(root->left);
+        root ->right = invertTree(root->right);
+        swap(root->left, root->right);
+        return root;
+    }
+};
