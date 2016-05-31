a744e835ad91c0422ce06c96c9dfe50e053e56c3
Yin Wen <240083998@qq.com>
2016-05-31 22:36:47 +0800

    去除字符串里的重复字母，并且使得结果为最小字典序

---
+++ Remove Duplicate Letters/version_1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int m[256] = {0}, visited[256] = {0};
+        string res = "0";
+        for (auto a : s) ++m[a];
+        for (auto a : s) {
+            --m[a];
+            if (visited[a]) continue;
+            while (a < res.back() && m[res.back()]) {
+                visited[res.back()] = 0;
+                res.pop_back();
+            }
+            res += a;
+            visited[a] = 1;
+        }
+        return res.substr(1);
+    }
+};
